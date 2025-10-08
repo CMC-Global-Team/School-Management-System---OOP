5f4bb63b37b4a71f69f9b399aef48d4ec5f20ef0
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-08 11:17:03 +0700

    create input util atribute

--- src/Utils/InputUtil.java
+++ src/Utils/InputUtil.java
@@ -1,5 +1,16 @@
 package Utils;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class InputUtil {
-    
+    private static Scanner scanner = new Scanner(System.in);
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    public static String getString(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine().trim();
+    }
 }
