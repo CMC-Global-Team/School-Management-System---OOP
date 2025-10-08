f31e68d0207066983d5b1e0683372d39b6303ea2
Tran Thanh <tienthanh6042@gmail.com>
2025-10-08 21:44:10 +0700

    Update AverageGradeScreen

--- src/Screen/Grade/AverageGradeScreen.java
+++ src/Screen/Grade/AverageGradeScreen.java
@@ -5,6 +5,7 @@ import Models.Student;
 import Models.Subject;
 import Screen.AbstractScreen;
 import Services.GradeServices;
+import Services.StudentService;
 import Utils.FileUtil;
 import Utils.InputUtil;
 
@@ -22,7 +23,17 @@ public class AverageGradeScreen extends AbstractScreen{
 
     @Override
     public void handleInput() {
+        if (GradeServices.getInstance().getTotalGradeRecords() == 0) {
+            System.out.println("\nKhông có bản ghi nào để cập nhật.");
+            pause();
+            return;
+        }
         String studentID = InputUtil.getNonEmptyString("Mã học sinh: ");
-        GradeServices.getInstance().AverageScore(studentID);
+        if(StudentService.getInstance().isStudentIdExists(studentID)){
+            GradeServices.getInstance().AverageScore(studentID);
+        }else {
+            System.out.println("Không tìm thấy mã học sinh: " + studentID);
+        }
+
     }
 }
