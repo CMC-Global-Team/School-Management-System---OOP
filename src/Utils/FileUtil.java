efca0fb4b30036b796051e10bae48f524ccac936
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-08 11:14:53 +0700

    file util add writeLines to file

--- src/Utils/FileUtil.java
+++ src/Utils/FileUtil.java
@@ -1,5 +1,17 @@
 package Utils;
 
+import java.io.*;
+import java.util.List;
+
 public class FileUtil {
-    
+    // Ghi danh sách String vào file
+    public static void writeLines(String filePath, List<String> lines) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+    }
+
 }
