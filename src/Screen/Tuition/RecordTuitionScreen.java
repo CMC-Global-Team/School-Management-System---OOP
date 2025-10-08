723c54fd6192a37b07ff7bde4a22296022c8e869
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-08 14:43:43 +0700

    add input for tuitionid,studentid,semester,schoolYear,amount,status,method,note

--- src/Screen/Tuition/RecordTuitionScreen.java
+++ src/Screen/Tuition/RecordTuitionScreen.java
@@ -17,7 +17,14 @@ public class RecordTuitionScreen extends AbstractScreen {
     }
     @Override
     public void handleInput() {
-
+        String tuitionId = InputUtil.getNonEmptyString("Nhập mã học phí: ");
+        String studentId = InputUtil.getNonEmptyString("Mã học sinh: ");
+        int semester = InputUtil.getInt("Học kỳ: ");
+        String schoolYear = InputUtil.getNonEmptyString("Năm học: ");
+        double amount = InputUtil.getDouble("Số tiền thu: ");
+        String status = InputUtil.getNonEmptyString("Trạng thái: ");
+        String method = InputUtil.getNonEmptyString("Phương thức thanh toán: ");
+        String note = InputUtil.getString("Ghi chú (nếu có): ");
     }
 }
 
