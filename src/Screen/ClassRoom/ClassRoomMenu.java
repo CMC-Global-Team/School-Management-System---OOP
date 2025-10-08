4552c35080c883b96af816754745223e17134687
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-08 11:52:11 +0700

    craete attribute classroom menu

---
+++ src/Screen/ClassRoom/ClassRoomMenu.java
@@ -0,0 +1,11 @@
+package Screen.ClassRoom;
+
+import Screen.AbstractScreen;
+
+public class ClassRoomMenu extends AbstractScreen {
+    private final AddClassRoomScreen addScreen;
+    private final SearchClassRoomScreen searchScreen;
+    private final DeleteClassRoomScreen deleteScreen;
+    private final UpdateClassRoomScreen updateScreen;
+    private final ListClassRoomScreen listScreen;
+}
