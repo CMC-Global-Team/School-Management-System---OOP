660da7eaa45449dec6516a4686c9f3b481b18a8b
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-08 16:12:36 +0700

    create tution service with instance and attribute

--- src/Services/TuitionService.java
+++ src/Services/TuitionService.java
@@ -1,39 +1,35 @@
 package Services;
 
 import Models.Tuition;
+import Utils.InputUtil;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 
 public class TuitionService {
     private static TuitionService instance;
     private final TuitionRepository repository;
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     private final StudentRepository studentRepository;
 
     private TuitionService() {
         this.repository = new TuitionRepository();
+
         this.studentRepository = new StudentRepository();
     }
 
+    /**
+     * Lấy instance duy nhất của TuitionService (Singleton)
+     */
     public static TuitionService getInstance() {
         if (instance == null) {
             instance = new TuitionService();
         }
         return instance;
-    }
-
-    public List<Tuition> getAllTuitions() {
-        return repository.findAll();
-    }
-
-    public Optional<Tuition> findById(String tuitionId) {
-        return repository.findById(tuitionId);
-    }
-
-    public boolean isTuitionIdExists(String tuitionId) {
-        return repository.exists(tuitionId);
-    }
-
-    public int getTotalTuitions() {
-        return repository.count();
     }
 }
