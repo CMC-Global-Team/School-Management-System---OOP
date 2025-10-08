981eee175a9fc5bed5702bec6b607ce6a42343dd
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-08 14:54:52 +0700

    add display layout for EditTuitionScreen

---
+++ src/Screen/Tuition/EditTuitionScreen.java
@@ -0,0 +1,21 @@
+package Screen.Tuition;
+import Models.Tuition;
+import Screen.AbstractScreen;
+
+/**
+ * EditTuitionScreen - Màn hình chỉnh sửa thông tin học phí
+ *
+ */
+public class EditTuitionScreen extends AbstractScreen {
+
+
+    @Override
+    public void display() {
+        System.out.println("┌──────────────────────────────────────────┐");
+        System.out.println("│         Sửa Thông Tin Học Phí            │");
+        System.out.println("└──────────────────────────────────────────┘");
+    }
+    public void handleInput() {
+
+    }
+}
