2d529a50597f8d2b5026672b9e8e1f0b36343ee3
Tran Thanh <tienthanh6042@gmail.com>
2025-10-08 14:37:55 +0700

    Create íntance

--- src/Services/GradeServices.java
+++ src/Services/GradeServices.java
@@ -1,4 +1,20 @@
 package Services;
 
 public class GradeServices {
+    private static GradeServices instance;
+    private final GradeRepository repository;
+
+    private GradeServices() {
+        this.repository = new GradeRepository();
+    }
+
+    /**
+     * Lấy instance duy nhất của ClassroomService (Singleton)
+     */
+    public static GradeServices getInstance() {
+        if (instance == null) {
+            instance = new GradeServices();
+        }
+        return instance;
+    }
 }
